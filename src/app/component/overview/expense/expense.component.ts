062a8bf5374521393e372adee462996c6d6bcd79
Sali's Code <siljjazi28@gmail.com>
2025-03-10 16:43:17 +0100

    Added the Expense component that it is used to track the expenses of the user

--- src/app/component/overview/expense/expense.component.ts
+++ src/app/component/overview/expense/expense.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-expense',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule
+    ],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss'
 })
 export class ExpenseComponent {
-
+  choices = ['housing', 'transportation', 'food & groceries', 'health and insurance', 'entertainment', 'clothing', 'vacations', 'other'];
+  selectedOption = this.choices[0];
 }
