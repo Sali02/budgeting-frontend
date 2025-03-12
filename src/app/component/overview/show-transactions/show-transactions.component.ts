27d3ac318e91e958f9aca712417eb1905c022b4c
Sali's Code <siljjazi28@gmail.com>
2025-03-12 01:10:05 +0100

    Added the code for the show-transaction file and shows the last 10 transactions the user made.

--- src/app/component/overview/show-transactions/show-transactions.component.ts
+++ src/app/component/overview/show-transactions/show-transactions.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import data from "../../../../asset/dummy-data.json";
+import {DatePipe, DecimalPipe} from "@angular/common";
 
 @Component({
   selector: 'app-show-transactions',
   standalone: true,
-  imports: [],
+  imports: [
+    DecimalPipe,
+    DatePipe
+  ],
   templateUrl: './show-transactions.component.html',
   styleUrl: './show-transactions.component.scss'
 })
 export class ShowTransactionsComponent {
 
+  transactions = data.DUMMY_DATA;
+
+  constructor() {
+  }
+
+
+  protected readonly data = data;
 }
